cb57f98913f9f95448f732e9fec898af77e9fa06
Gabriel <gabrieltlaltilolpa@gmail.com>
2023-08-10 22:04:20 -0600

    Componentes, rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './Components/inicio/inicio.component';
+import { RouterModule } from '@angular/router';
+import { MenuComponent } from './Components/menu/menu.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    MenuComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path:'inicio',
+        component: InicioComponent
+      },
+      {
+        path:'menu',
+        component: MenuComponent
+      }
+  ]),
+    MatIconModule,
+    MatDividerModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
